9dab3d33d06304a9175b648904d98ae9bcefe9c6
Joshua <legodude17@users.noreply.github.com>
2021-07-27 17:25:56 -0700

    Redundant stomach

---
+++ Source/VFEAncients/PowerWorker_Hunger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEAncients
+{
+    public class PowerWorker_Hunger : PowerWorker
+    {
+        public PowerWorker_Hunger(PowerDef def) : base(def)
+        {
+        }
+
+        public override void DoPatches(Harmony harm)
+        {
+            base.DoPatches(harm);
+            harm.Patch(AccessTools.PropertyGetter(typeof(Need_Food), nameof(Need_Food.GUIChangeArrow)), new HarmonyMethod(GetType(), nameof(HungerArrow)));
+            harm.Patch(AccessTools.Method(typeof(Need_Food), nameof(Need_Food.NeedInterval)), new HarmonyMethod(GetType(), nameof(Interval)));
+        }
+
+        public static bool HungerArrow(Need_Food __instance, ref int __result, Pawn ___pawn)
+        {
+            if (HasPower<PowerWorker_Hunger>(___pawn))
+            {
+                __result = Math.Abs(__instance.CurLevel - __instance.MaxLevel) < 0.009f ? 0 : 1;
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Interval(Need_Food __instance, Pawn ___pawn)
+        {
+            if (HasPower<PowerWorker_Hunger>(___pawn)) __instance.CurLevel += __instance.FoodFallPerTick * 300f;
+        }
+    }
+}
