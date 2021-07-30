bb560c45f427b8604944194264f4ae6774e72e7b
Joshua <legodude17@users.noreply.github.com>
2021-07-30 15:06:45 -0700

    Darkvision

---
+++ Source/VFEAncients/PowerWorker_PrefersDarkness.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEAncients
+{
+    public class PowerWorker_PrefersDarkness : PowerWorker
+    {
+        public PowerWorker_PrefersDarkness(PowerDef def) : base(def)
+        {
+        }
+
+        public override void DoPatches(Harmony harm)
+        {
+            base.DoPatches(harm);
+            harm.Patch(AccessTools.Method(typeof(StatPart_Glow), "ActiveFor"), postfix: new HarmonyMethod(GetType(), nameof(Glow_ActiveFor_Postfix)));
+        }
+
+        public static void Glow_ActiveFor_Postfix(Thing t, ref bool __result)
+        {
+            if (__result && HasPower<PowerWorker_PrefersDarkness>(t)) __result = false;
+        }
+    }
+}
