ab8dacf8823d6e7fb8493509c90bbdc063d50507
Joshua <legodude17@users.noreply.github.com>
2021-08-06 23:52:24 -0700

    Bloodlust

---
+++ Source/VFEAncients/PowerWorker_BreakOnKilled.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEAncients
+{
+    public class PowerWorker_BreakOnKilled : PowerWorker
+    {
+        public PowerWorker_BreakOnKilled(PowerDef def) : base(def)
+        {
+        }
+
+        public override void DoPatches(Harmony harm)
+        {
+            base.DoPatches(harm);
+            harm.Patch(AccessTools.Method(typeof(PawnDiedOrDownedThoughtsUtility), "AppendThoughts_ForHumanlike"),
+                transpiler: new HarmonyMethod(GetType(), nameof(InjectOnKilled)));
+        }
+
+        public static IEnumerable<CodeInstruction> InjectOnKilled(IEnumerable<CodeInstruction> instructions)
+        {
+            var list = instructions.ToList();
+            var idx1 = list.FindIndex(ins => ins.opcode == OpCodes.Ldloc_2);
+            list.InsertRange(idx1 + 1, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_2),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PowerWorker_BreakOnKilled), nameof(OnKilled)))
+            });
+            return list;
+        }
+
+        public static void OnKilled(Pawn killer, Pawn killed)
+        {
+            if (HasPower<PowerWorker_BreakOnKilled>(killer))
+            {
+                var data = GetData<WorkerData_Break>(killer);
+                if (data != null && Rand.Chance(data.BreakChance)) data.Break.Worker.TryStart(killer, ThoughtDefOf.KilledHumanlikeBloodlust.stages[0].LabelCap, false);
+            }
+        }
+    }
+}
