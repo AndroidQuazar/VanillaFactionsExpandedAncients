cf39f98965cd45aabde186cd086ce7c730d00783
Joshua <legodude17@users.noreply.github.com>
2021-08-07 19:02:46 -0700

    Zealot

---
+++ Source/VFEAncients/JobDriver_ZealotExecution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace VFEAncients
+{
+    internal class JobDriver_ZealotExecution : JobDriver
+    {
+        protected Pawn Victim => (Pawn) job.targetA.Thing;
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return pawn.Reserve(Victim, job, 1, -1, null, errorOnFailed);
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnAggroMentalState(TargetIndex.A);
+            this.FailOnDestroyedOrNull(TargetIndex.A);
+            yield return new Toil
+            {
+                initAction = delegate { pawn.pather.StartPath(Victim, PathEndMode.Touch); },
+                defaultCompleteMode = ToilCompleteMode.PatherArrival,
+                socialMode = RandomSocialMode.Off
+            };
+            yield return new Toil
+            {
+                initAction = delegate
+                {
+                    ExecutionUtility.DoExecutionByCut(pawn, Victim);
+                    ThoughtUtility.GiveThoughtsForPawnExecuted(Victim, pawn, PawnExecutionKind.GenericBrutal);
+                    TaleRecorder.RecordTale(TaleDefOf.ExecutedPrisoner, pawn, Victim);
+                },
+                defaultCompleteMode = ToilCompleteMode.Instant,
+                activeSkill = () => SkillDefOf.Melee
+            };
+        }
+    }
+}
---
+++ Source/VFEAncients/ThinkNode_ConditionalPower.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+using Verse.AI;
+
+namespace VFEAncients
+{
+    public class ThinkNode_ConditionalPower : ThinkNode_Conditional
+    {
+        public PowerDef Power;
+
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return pawn?.GetPowerTracker()?.HasPower(Power) ?? false;
+        }
+    }
+}
