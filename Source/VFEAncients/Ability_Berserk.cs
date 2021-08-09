da4d18c0b6060ac5cefbd0ef0efcc382913d49a8
Joshua <legodude17@users.noreply.github.com>
2021-08-09 13:07:28 -0700

    Telepathy

---
+++ Source/VFEAncients/Ability_Berserk.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+using Ability = VFECore.Abilities.Ability;
+
+namespace VFEAncients
+{
+    public class Ability_Berserk : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            if (target.HasThing && target.Thing is Pawn)
+                target.Pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Berserk, null, true, false, null, false, false, true);
+        }
+    }
+}
---
+++ Source/VFEAncients/Ability_Calm.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using VFECore.Abilities;
+
+namespace VFEAncients
+{
+    public class Ability_Calm : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            target.Pawn?.MentalState?.RecoverFromState();
+        }
+
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            return base.ValidateTarget(target, showMessages) && target.HasThing && target.Thing is Pawn p && p.InMentalState;
+        }
+    }
+}
