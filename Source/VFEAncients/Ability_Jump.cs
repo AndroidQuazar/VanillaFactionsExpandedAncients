0bb5bbd2ddaa021a0fb4f5aed8cfeb812cd22f12
Joshua <legodude17@users.noreply.github.com>
2021-08-09 12:53:19 -0700

    Super jump

---
+++ Source/VFEAncients/Ability_Jump.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+using VFECore.Abilities;
+using Ability = VFECore.Abilities.Ability;
+
+namespace VFEAncients
+{
+    public class Ability_Jump : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            var map = Caster.Map;
+            var flyer = (AbilityPawnFlyer) PawnFlyer.MakeFlyer(VFE_DefOf_Abilities.VFEA_AbilityFlyer, CasterPawn, target.Cell);
+            flyer.ability = this;
+            flyer.target = target.CenterVector3;
+            GenSpawn.Spawn(flyer, Caster.Position, map);
+
+            base.Cast(target);
+        }
+    }
+}
