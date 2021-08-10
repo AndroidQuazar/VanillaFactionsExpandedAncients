96d3ed7d3ce82958d630656271f91bdb07a0cecd
Joshua <legodude17@users.noreply.github.com>
2021-08-10 16:47:38 -0700

    Heal others

---
+++ Source/VFEAncients/Ability_Heal.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Verse;
+
+namespace VFEAncients
+{
+    public class Ability_Heal : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            if (target.Pawn != null)
+                foreach (var injury in target.Pawn.health.hediffSet.hediffs.OfType<Hediff_Injury>().ToList())
+                    target.Pawn.health.RemoveHediff(injury);
+        }
+    }
+}
