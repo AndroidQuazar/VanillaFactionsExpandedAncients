c5dcc0209b894a9f734101774644592ad244815e
Joshua <legodude17@users.noreply.github.com>
2021-08-10 17:54:25 -0700

    EMP

---
+++ Source/VFEAncients/Ability_Explode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace VFEAncients
+{
+    public class Ability_Explode : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            if (def.TryGetModExtension<AbilityExtension_Explosion>(out var ext))
+                GenExplosion.DoExplosion(ext.onCaster ? pawn.Position : target.Cell, pawn.Map, ext.explosionRadius, ext.explosionDamageDef, pawn, ext.explosionDamageAmount,
+                    intendedTarget: ext.onCaster ? null : target.Thing, ignoredThings: new List<Thing> {pawn});
+        }
+    }
+
+    public class AbilityExtension_Explosion : DefModExtension
+    {
+        public int explosionDamageAmount;
+        public DamageDef explosionDamageDef;
+        public float explosionRadius;
+        public bool onCaster;
+    }
+}
