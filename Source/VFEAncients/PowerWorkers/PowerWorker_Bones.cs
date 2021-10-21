7685f5bbf34a8162803097cbd1de3d663c6653dc
Joshua <legodude17@users.noreply.github.com>
2021-10-20 17:02:07 -0700

    Fix plasteel skeleton

--- Source/VFEAncients/PowerWorkers/PowerWorker_Bones.cs
+++ Source/VFEAncients/PowerWorkers/PowerWorker_Bones.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Verse;
 
 namespace VFEAncients
@@ -15,78 +12,18 @@ namespace VFEAncients
         public override void DoPatches(Harmony harm)
         {
             base.DoPatches(harm);
-            harm.Patch(AccessTools.Method(typeof(DamageWorker_AddInjury), "ApplyDamageToPart"), transpiler: new HarmonyMethod(GetType(), nameof(ApplyDamageToPart_Transpile)));
+            harm.Patch(
+                AccessTools.Method(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.AddHediff),
+                    new[] {typeof(Hediff), typeof(BodyPartRecord), typeof(DamageInfo), typeof(DamageWorker.DamageResult)}),
+                new HarmonyMethod(GetType(), nameof(HandleBones)));
         }
 
-        public static IEnumerable<CodeInstruction> ApplyDamageToPart_Transpile(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        public static bool HandleBones(Pawn_HealthTracker __instance, Hediff hediff, BodyPartRecord part = null, DamageInfo? dinfo = null, DamageWorker.DamageResult result = null)
         {
-            var list = instructions.ToList();
-            var info1 = AccessTools.Method(typeof(DamageInfo), nameof(DamageInfo.SetHitPart));
-            var idx1 = list.FindIndex(ins => ins.Calls(info1));
-            var label1 = generator.DefineLabel();
-            list[idx1 + 1].labels.Add(label1);
-            // list.InsertRange(idx1 + 1, new[]
-            // {
-            //     new CodeInstruction(OpCodes.Ldarg_2),
-            //     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PowerWorker), nameof(HasPower), generics: new[] {typeof(PowerWorker_Bones)})),
-            //     new CodeInstruction(OpCodes.Brfalse, label1),
-            //     new CodeInstruction(OpCodes.Ldloc_2),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BodyPartRecord), nameof(BodyPartRecord.def))),
-            //     new CodeInstruction(OpCodes.Ldloc_2),
-            //     new CodeInstruction(OpCodes.Ldarg_2),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn), nameof(Pawn.health))),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.hediffSet))),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(HediffSet), nameof(HediffSet.hediffs))),
-            //     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(BodyPartDef), nameof(BodyPartDef.IsSolid))),
-            //     new CodeInstruction(OpCodes.Brfalse, label1),
-            //     new CodeInstruction(OpCodes.Ldarg_2),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn), nameof(Pawn.health))),
-            //     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.hediffSet))),
-            //     new CodeInstruction(OpCodes.Ldloc_2),
-            //     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(HediffSet), nameof(HediffSet.PartOrAnyAncestorHasDirectlyAddedParts))),
-            //     new CodeInstruction(OpCodes.Brtrue, label1),
-            //     new CodeInstruction(OpCodes.Ldarg_3),
-            //     new CodeInstruction(OpCodes.Ldarg_2),
-            //     new CodeInstruction(OpCodes.Ldloc_2),
-            //     new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(DamageWorker.DamageResult), nameof(DamageWorker.DamageResult.AddPart))),
-            //     new CodeInstruction(OpCodes.Ldarg_3),
-            //     new CodeInstruction(OpCodes.Ldc_I4_1),
-            //     new CodeInstruction(OpCodes.Stfld, AccessTools.Field(typeof(DamageWorker.DamageResult), nameof(DamageWorker.DamageResult.deflected))),
-            //     new CodeInstruction(OpCodes.Ret)
-            // });
-            list.InsertRange(idx1 + 1, new[]
-            {
-                new CodeInstruction(OpCodes.Ldloc_2),
-                new CodeInstruction(OpCodes.Ldarg_2),
-                new CodeInstruction(OpCodes.Ldarg_3),
-                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PowerWorker_Bones), nameof(HandleBones))),
-                new CodeInstruction(OpCodes.Brfalse, label1),
-                new CodeInstruction(OpCodes.Ret)
-            });
-            return list;
-        }
-
-        public static bool HandleBones(BodyPartRecord part, Pawn pawn, DamageWorker.DamageResult result)
-        {
-            Log.Message($"{pawn} has {part} hit");
-            if (HasPower<PowerWorker_Bones>(pawn))
-            {
-                Log.Message($"{pawn} has power!");
-                if (part.def.IsSolid(part, pawn.health.hediffSet.hediffs))
-                {
-                    Log.Message($"{part.def} is solid");
-                    if (!pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(part))
-                    {
-                        Log.Message($"{part} is not a prosthetic");
-                        result.AddPart(pawn, part);
-                        result.deflected = true;
-                        result.deflectedByMetalArmor = true;
-                        return true;
-                    }
-                }
-            }
-
-            return false;
+            var pawn = __instance.pawn;
+            part ??= hediff.Part;
+            return hediff is not Hediff_Injury || dinfo is not {Def: {hediffSolid: var hediffDef}} || hediffDef != hediff.def || !HasPower<PowerWorker_Bones>(pawn) ||
+                   !part.def.IsSolid(part, pawn.health.hediffSet.hediffs) || pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(part);
         }
     }
 }
