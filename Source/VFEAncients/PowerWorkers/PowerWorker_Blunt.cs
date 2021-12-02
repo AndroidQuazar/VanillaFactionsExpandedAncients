93594e246d25d77d5409739ad7d37149b2b58cd5
Joshua <legodude17@users.noreply.github.com>
2021-12-02 01:12:31 -0800

    Fix armor application with bulletproof skin

--- Source/VFEAncients/PowerWorkers/PowerWorker_Blunt.cs
+++ Source/VFEAncients/PowerWorkers/PowerWorker_Blunt.cs
@@ -13,17 +13,22 @@ namespace VFEAncients
         public override void DoPatches(Harmony harm)
         {
             base.DoPatches(harm);
-            harm.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.PreApplyDamage)), postfix: new HarmonyMethod(GetType(), nameof(ChangeType)));
+            harm.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.PreApplyDamage)), postfix: new HarmonyMethod(GetType(), nameof(SurfaceOnly)));
+            harm.Patch(AccessTools.Method(typeof(ArmorUtility), nameof(ArmorUtility.GetPostArmorDamage)), postfix: new HarmonyMethod(GetType(), nameof(ChangeType)));
         }
 
-        public static void ChangeType(Pawn __instance, ref DamageInfo dinfo)
+        public static void SurfaceOnly(Pawn __instance, ref DamageInfo dinfo)
         {
             if (dinfo.Def != DamageDefOf.Blunt && dinfo.Def.armorCategory == DamageArmorCategoryDefOf.Sharp && HasPower<PowerWorker_Blunt>(__instance))
             {
-                dinfo.Def = DamageDefOf.Blunt;
                 dinfo.SetBodyRegion(depth: BodyPartDepth.Outside);
                 dinfo.SetAllowDamagePropagation(false);
             }
+        }
+
+        public static void ChangeType(Pawn pawn, ref DamageDef damageDef)
+        {
+            if (damageDef != DamageDefOf.Blunt && damageDef.armorCategory == DamageArmorCategoryDefOf.Sharp && HasPower<PowerWorker_Blunt>(pawn)) damageDef = DamageDefOf.Blunt;
         }
     }
 }
