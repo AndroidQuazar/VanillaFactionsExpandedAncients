eb4a45559622f9d6d1cf852299e56225f03f8d7c
Joshua <legodude17@users.noreply.github.com>
2022-07-31 06:49:28 -0700

    Fix self-control not showing at right time

--- Source/VFEAncients/Abilities/Ability_Calm.cs
+++ Source/VFEAncients/Abilities/Ability_Calm.cs
@@ -1,23 +1,25 @@
 ﻿using RimWorld;
 using RimWorld.Planet;
 using Verse;
+using VFECore.Abilities;
 
-namespace VFEAncients
+namespace VFEAncients;
+
+public class Ability_Calm : Ability
 {
-    public class Ability_Calm : Ability
+    public override void Cast(params GlobalTargetInfo[] targets)
     {
-        public override void Cast(params GlobalTargetInfo[] targets)
-        {
-            base.Cast(targets);
-            foreach (var target in targets) (target.Thing as Pawn)?.MentalState?.RecoverFromState();
-        }
+        base.Cast(targets);
+        foreach (var target in targets) (target.Thing as Pawn)?.MentalState?.RecoverFromState();
+    }
 
-        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
-        {
-            if (!base.ValidateTarget(target, showMessages) || !target.HasThing || !(target.Thing is Pawn p)) return false;
-            if (p.InMentalState) return true;
-            if (showMessages) Messages.Message("VFEAncients.NotInMentalState".Translate(), MessageTypeDefOf.RejectInput);
-            return false;
-        }
+    public override bool ShowGizmoOnPawn() => def.targetModes[0] == AbilityTargetingMode.Self ? pawn.InMentalState : base.ShowGizmoOnPawn();
+
+    public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+    {
+        if (!base.ValidateTarget(target, showMessages) || !target.HasThing || target.Thing is not Pawn p) return false;
+        if (p.InMentalState) return true;
+        if (showMessages) Messages.Message("VFEAncients.NotInMentalState".Translate(), MessageTypeDefOf.RejectInput);
+        return false;
     }
 }
